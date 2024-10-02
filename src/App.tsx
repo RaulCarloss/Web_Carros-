f8dbb8f83d30b71bddb1f7378c168c11ba804964
Raul Carlos <raulcarlos96@hotmail.com>
2024-10-02 12:10:46 -0300

    feat: add router for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-function App() {
-  return (
-    <div>
-      <h1>web caros</h1>
-    </div>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/home/home.page";
+import { Login } from "./pages/login/login.page";
+import { Register } from "./pages/register/register.page";
+import { Dashboard } from "./pages/dashboard/dashboard.page";
+import { New } from "./pages/dashboard/new/new.page";
+import { CarDetail } from "./pages/car/car.page";
 
-export default App;
+import { Layout } from "./components/layout/layout.component";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/car/:id",
+        element: <CarDetail />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/new",
+        element: <New />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export { router };
