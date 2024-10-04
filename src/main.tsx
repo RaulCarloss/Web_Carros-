2005d41c7b043079d43d89899d6f472d763d5626
Raul Carlos <raulcarlos96@hotmail.com>
2024-10-04 12:32:43 -0300

    feat: add authentication user

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { router } from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./contexts/authContext";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
 );
